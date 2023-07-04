fac3aabe389cf6099b072679080023e33f14fa2e
Aleksandra Nazhestkina <splinatus@yandex.ru>
2023-07-04 14:17:36 +0300

    redirect защищенного роута на /

--- my-app/src/utils/constants.js
+++ my-app/src/utils/constants.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "http://localhost:3001";
-export const BASE_URL = "https://api.nazhestkina.movies.nomoredomains.club";
+export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "https://api.nazhestkina.movies.nomoredomains.club";
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const SHORT_MOVIES_DURATION = 40;
 // export const EMAIL_PATTERN =
