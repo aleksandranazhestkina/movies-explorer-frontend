b63087738f9e06d9184f04d00ee3db08ba7c33ad
Aleksandra Nazhestkina <splinatus@yandex.ru>
2023-07-04 15:44:07 +0300

    поменять обратно BASE_URL

--- my-app/src/utils/constants.js
+++ my-app/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001";
-// export const BASE_URL = "https://api.nazhestkina.movies.nomoredomains.club";
+// export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://api.nazhestkina.movies.nomoredomains.club";
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 export const SHORT_MOVIES_DURATION = 40;
